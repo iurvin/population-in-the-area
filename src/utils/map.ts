afcb3c6bea528334b1313aed075e892723ecc7b9
yuvinokurov <yuvinokurov@ut-it.ru>
2024-02-03 09:28:45 +0300

    [add-interaction]: types

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,10 +1,10 @@
 import {transform} from "ol/proj";
 
-// @ts-ignore
-export function to4326(coord: Array<number> | Array<Array<number>>) {
+export function to4326(coord: number[] | number[][]): number[] | number[][]  {
   if (typeof coord[0] === 'number') {
     return transform(coord as Array<number>, 'EPSG:3857', 'EPSG:4326');
   }
+
   // @ts-ignore
   return coord.map((c) => to4326(c));
 }
